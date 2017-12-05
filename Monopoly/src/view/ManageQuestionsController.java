82b0770ff5c9e347a25c3adc9d854c9c7573b800
Enav Kasaon <ekasaon@campus.haifa.ac.il>
2017-12-06 00:18:11 +0200

    nowwwwwwww

--- Monopoly/src/view/ManageQuestionsController.java
+++ Monopoly/src/view/ManageQuestionsController.java
@@ -16,28 +16,42 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class ManageQuestionsController implements Initializable{
 	
 	@FXML
     private ComboBox subjects ;
+	@FXML
+	private ComboBox level ;
+	@FXML
+	private CheckBox IsMultipleChoice;
+	@FXML
+	private Pane AnsPane;
 	
 	/**
-	 * get all subjects for questions
+	 * get answers if multiple
 	 * */
-	public void hundleSubjects(ActionEvent event) throws IOException{
-	ObservableList obList = FXCollections.observableArrayList(AdminControl.getSubjects());
-	subjects.getItems().clear();
-	subjects.setItems(obList);
+	
+	public void hundleMultiple(ActionEvent event) throws IOException{
+		if(IsMultipleChoice.isSelected())
+			AnsPane.setVisible(true);
+		else
+			AnsPane.setVisible(false);
 	}
 	
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
-		
+	//	subjects.getItems().removeAll(subjects.getItems());
+		subjects.getItems().addAll(AdminControl.getSubjects());
+	//	level.getItems().removeAll(level.getItems());
+		level.getItems().addAll("1", "2", "3");
+		AnsPane.setVisible(false);
 	}
 
 }
