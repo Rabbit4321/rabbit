40180a08118f34ba4a77a2d682a97692229a6a6f
Enav Kasaon <ekasaon@campus.haifa.ac.il>
2017-12-06 16:53:13 +0200

    enav last commit 6/12 - 16:53

--- Monopoly/src/control/AdminControl.java
+++ Monopoly/src/control/AdminControl.java
@@ -2,15 +2,34 @@ package control;
 
 
 
+import java.util.ArrayList;
+
+import javax.security.auth.Subject;
+
+import model.Admin;
+import model.Answer;
 import model.Question;
 import model.SysData;
 import model.Subjects;
 
 public class AdminControl {
+	
+	private static Admin a= new Admin(); // not supposed to be here - just for trying
 
 	
-	public static boolean addQuestion(Question q){//not finished
-		return false;
+	public static boolean addQuestion(int id, String team, String text, int diff, boolean isMulti,Answer ans1,Answer ans2,Answer ans3,Answer ans4, ArrayList<String> tags){//not finished
+	 ArrayList<Subjects> sub = new ArrayList<Subjects>();
+	 for(String s : tags) {
+		 sub.add(Subjects.valueOf(s));
+	 }
+	 ArrayList<Answer> answers = new ArrayList<Answer>();
+	 answers.add(ans1);
+	 answers.add(ans2);
+	 answers.add(ans3);
+	 answers.add(ans4);
+	 
+	 a.AddQuestion(id, null, text, answers, diff, isMulti, sub);
+	 return true;
 		
 	}
 	
@@ -24,9 +43,21 @@ public class AdminControl {
 	public static Subjects[] getSubjects() {
 		return SysData.getInstance().getSubjects();	
 	}
-	public static void main(String[] args) {
-		for (int i =0; i<AdminControl.getSubjects().length;i++) {
-			System.out.println(AdminControl.getSubjects()[i].toString());
-		}
+	
+	/**
+	 * check if Admin
+	 * */
+	public static boolean CheckIfAdmin(String id, String pass) {
+
+		if(a.getID().equals(id) && a.getPassword().equals(pass))
+			return true;
+		return false;
+	}
+	/**
+	 * create answer
+	 * */
+	public static Answer CreateAns(String text, boolean isCorrect) {
+		Answer ans = new Answer (text,isCorrect);
+		return ans;
 	}
 }
--- Monopoly/src/view/ManageQuestionsController.java
+++ Monopoly/src/view/ManageQuestionsController.java
@@ -2,11 +2,15 @@ package view;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 
 
 import control.AdminControl;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -16,9 +20,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -32,26 +42,159 @@ public class ManageQuestionsController implements Initializable{
 	private CheckBox IsMultipleChoice;
 	@FXML
 	private Pane AnsPane;
+	@FXML
+	private Pane OneAnswer;
+	@FXML
+	private RadioButton ans1;
+	@FXML
+	private RadioButton ans2;
+	@FXML
+	private RadioButton ans3;
+	@FXML
+	private RadioButton ans4;
+	@FXML
+	private ToggleGroup group;
+	@FXML
+	private ListView<String> ListSubjects;
+	@FXML
+	private TextField ID;
+	@FXML
+	private TextField Question;
+	@FXML
+	private TextField a1;
+	@FXML
+	private TextField a2;
+	@FXML
+	private TextField a3;
+	@FXML
+	private TextField a4;
+	@FXML
+	private TextField OneAns;
 	
 	/**
 	 * get answers if multiple
 	 * */
-	
+	@FXML
 	public void hundleMultiple(ActionEvent event) throws IOException{
-		if(IsMultipleChoice.isSelected())
+		if(IsMultipleChoice.isSelected()) {
 			AnsPane.setVisible(true);
-		else
+			OneAnswer.setVisible(false);
+			ans1.setVisible(true);
+			ans2.setVisible(true);
+			ans3.setVisible(true);
+			ans4.setVisible(true);
+		}
+		else {
 			AnsPane.setVisible(false);
+			OneAnswer.setVisible(true);
+			ans1.setVisible(false);
+			ans2.setVisible(false);
+			ans3.setVisible(false);
+			ans4.setVisible(false);
+		}
 	}
 	
+	/**
+	 * fill list of subjects
+	 * */
+	@FXML
+	public void hundleSubjects(ActionEvent event) throws IOException{
+		ObservableList<String> sub = ListSubjects.getItems();
+		if(!sub.contains(subjects.getValue().toString())){
+		sub.add(subjects.getValue().toString());
+		ListSubjects.setItems(sub);
+		}
+	}
+	/**
+	 * 
+	 * fields validation and insert
+	 * */
+	@FXML
+	public void hundleAdd(ActionEvent event) throws IOException{
+		int id = Integer.parseInt(ID.getText());
+		String q = Question.getText();
+		int diff = Integer.parseInt(level.getValue().toString());
+		boolean isMulti = IsMultipleChoice.isSelected();
+		ListSubjects.getItems();
+		List<String> su = ListSubjects.getItems();
+	    ArrayList<String> showing = new ArrayList<String>();
+	    for(String s: ListSubjects.getItems() ) {
+	    	showing.add(s);
+	    }
+		if(isMulti) {
+		
+			if(AdminControl.addQuestion(id, null, q, diff, isMulti, AdminControl.CreateAns(a1.getText(),ans1.isSelected()),
+					AdminControl.CreateAns(a2.getText(),ans2.isSelected()),
+					AdminControl.CreateAns(a3.getText(),ans3.isSelected()),
+					AdminControl.CreateAns(a4.getText(),ans4.isSelected()), showing)) 
+				AlertSuccess();
+			else 
+				AlertFailure();
+		}
+		else {
+			if(AdminControl.addQuestion(id, null, q, diff, isMulti, AdminControl.CreateAns(OneAns.getText(),true),
+					null,
+					null,
+					null, showing)) 
+				AlertSuccess();
+			else
+				AlertFailure();
+		}
+	}
+	@FXML
+	public void AlertSuccess() {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Information Dialog");
+		alert.setHeaderText(null);
+		alert.setContentText("Question Added Successfully!");
 
+		alert.showAndWait();
+		
+	}
+	@FXML
+	public void AlertFailure() {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Information Dialog");
+		alert.setHeaderText(null);
+		alert.setContentText("Can't Add Question!");
+
+		alert.showAndWait();
+	}
+	
+	/**
+	 * 
+	 * field validation
+	 * */
+	@FXML
+	public void Validate(ActionEvent event) throws IOException{
+		ID.textProperty().addListener(new ChangeListener<String>() {
+		    public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+		        if (!newValue.matches("\\d*")) {
+		            ID.setText(newValue.replaceAll("[^\\d]", ""));
+		        }
+		    }
+		});
+		
+	}
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-	//	subjects.getItems().removeAll(subjects.getItems());
 		subjects.getItems().addAll(AdminControl.getSubjects());
-	//	level.getItems().removeAll(level.getItems());
 		level.getItems().addAll("1", "2", "3");
 		AnsPane.setVisible(false);
+		OneAnswer.setVisible(true);
+		 group=new ToggleGroup();
+		 ans1.setToggleGroup(group);
+		 ans2.setToggleGroup(group);
+		 ans3.setToggleGroup(group);
+		 ans4.setToggleGroup(group);
+		 ID.textProperty().addListener(new ChangeListener<String>() {
+			    public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+			        if (!newValue.matches("\\d*")) {
+			            ID.setText(newValue.replaceAll("[^\\d]", ""));
+			        }
+			    }
+			});
 	}
 
 }
